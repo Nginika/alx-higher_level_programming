66f7653de8abbd80fddbd37a29815f73829b0845
Nginika <nginikaugochukwu@gmail.com>
2022-12-01 22:06:18 +0000

    print names from a module with dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
+i = 0
 if __name__ == "__main__":
     lists = sorted(dir(hidden_4))
-    for i in range(len(lists)):
+    while i < len(lists):
         if lists[i][0] != '_':
-            print(f"{lists[i]}")
+            print(lists[i])
+        i += 1
