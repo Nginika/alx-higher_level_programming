945725e6477b687bfea7e38330b84024395bbc52
Nginika <nginikaugochukwu@gmail.com>
2023-03-13 21:07:19 +0000

    using for loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const multiLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const length = multiLang.length;
+
+for (let i = 0; i < length; i++) {
+  console.log(multiLang[i]);
+}
