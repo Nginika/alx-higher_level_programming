7e54c4717fd9ba4f3e42a347d3ac9b5c578af195
Nginika <nginikaugochukwu@gmail.com>
2023-03-13 20:53:21 +0000

    using parseInt to convert to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+
+if (isNaN(args[2])) {
+  console.log('Not a number');
+} else {
+  const number = parseInt(args[2]);
+  console.log('My number: ' + number);
+}
